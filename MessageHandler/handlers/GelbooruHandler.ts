4b4e8c0ccad4b60965bdff44b135dd4f8ef7e0c1
ThisSilenceIsMine <aurakel542@gmail.com>
2025-10-07 00:50:07 +0300

    fix: :bug: Correct variable names and enhance logging in GelbooruHandler for improved clarity and debugging

--- MessageHandler/handlers/GelbooruHandler.ts
+++ MessageHandler/handlers/GelbooruHandler.ts
@@ -12,29 +12,36 @@ export class GelbooruHandler implements ContentHandler {
 
   async handle(msg: Message, bot: TelegramBot): Promise<PostResult> {
     const chatId = msg.chat.id;
-    const danbooruUrl = msg.text;
+    const gelbooruUrl = msg.text;
 
-    if (!danbooruUrl) {
+    if (!gelbooruUrl) {
       await bot.sendMessage(chatId, "Send me a link to a danbooru post.");
       return { error: "No danbooru URL provided" };
     }
 
-    const postInfo = await fetchGelbooruInfo(danbooruUrl);
+    console.log("getting post from", gelbooruUrl);
+    const postInfo = await fetchGelbooruInfo(gelbooruUrl);
 
+    console.log("postInfo", postInfo);
     if (!postInfo) {
       await bot.sendMessage(chatId, "Failed to fetch post info");
       return { error: "Failed to fetch post info" };
     }
 
+    console.log("fetching image stream");
     const imageStream = await fetchImageStream(postInfo.imageUrl);
+
+    console.log("imageStream", imageStream);
 
     if (!imageStream) {
       await bot.sendMessage(chatId, "Failed to create image stream");
       return { error: "Failed to create image stream" };
     }
 
+    console.log("building caption");
     const caption = buildCaption(postInfo);
 
+    console.log("caption", caption);
     // Check if the message has a spoiler flag or if the post has a NSFW rating
     let spoiler = msg.has_media_spoiler || isNSFW(postInfo.rating);
 
